89bd914d10c6a841d36abbf1c8bb5c2a2b762fa5
Rodney Smith <rodneysmith@usbr.gov>
2025-10-06 11:04:46 -0700

    Reverted previous commit changes

--- Logic.py
+++ Logic.py
@@ -67,6 +67,8 @@ def gapCheck(timestamps, data):
     return data
 
 def combineParameters(data, newData):
+    if len(data) != len(newData):
+        return data  # Mismatch—skip
     for d in range(len(newData)):
         parseLine = newData[d].split(',')
         data[d] = f'{data[d]},{parseLine[1]}'
@@ -473,7 +475,7 @@ class sortWorker(QRunnable):
 
         self.rows.sort(key=sort_key, reverse=not self.ascending)
         self.signals.sort_done.emit(self.rows, self.ascending)
-
+        
 def centerWindowToParent(ui):
     # Center window relative to parent (main window)
     if ui.parent():  # Fallback if no parent
