cd2fe348cab191d5ddb5525f077ab72b605d44ba
S31F3R <seifer@fyugaming.com>
2025-10-24 05:51:29 -0700

    Updated test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import Logic
 Logic.debug = True
 conn = Oracle.oracleConnection("LCHDB")
 conn.connect()
-result = conn.executeCustomQuery("SELECT SYSDATE FROM DUAL", params=None, fetchAll=False)
-#result = conn.executeCustomQuery("SELECT * FROM table WHERE id = :1", params=[1], fetchAll=True)
+#result = conn.executeCustomQuery("SELECT SYSDATE FROM DUAL", params=None, fetchAll=False)
+result = conn.executeCustomQuery("SELECT * FROM BLYTHE_DMDEI WHERE id = :1", params=[1], fetchAll=True)
 print(result)
 conn.close()
