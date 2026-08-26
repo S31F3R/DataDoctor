c55bc95750880591f5f357c968d16efe25084e4e
S31F3R <seiferfyu@protonmail.com>
2026-08-25 20:38:00 -0700

    Bump version to 3.0.0-rc.2.4

--- core/Version.py
+++ core/Version.py
@@ -15,7 +15,7 @@ from __future__ import annotations
 import re
 
 # Bump this when cutting a release (tag should match: v3.0.0 or v3.0.0-rc.1)
-VERSION = "3.0.0-rc.2.3"
+VERSION = "3.0.0-rc.2.4"
 
 # GitHub repo for release checks (owner/name)
 GITHUB_REPO = "S31F3R/DataDoctor"
